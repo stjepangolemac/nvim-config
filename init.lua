a426a4397452f5b4b2e6f74e2431bdf54efa567c
Stjepan Golemac <sgolem@dev-box.dev-box.local>
2021-06-18 12:47:38 +0200

    Fix auto install on the first start

--- init.lua
+++ init.lua
@@ -7,18 +7,27 @@ local fn = vim.fn
 local execute = vim.api.nvim_command
 
 -- Auto install packer.nvim if not exists
+local first_start = false
 local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
   execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+  first_start = true
 end
+
 vim.cmd [[packadd packer.nvim]]
-vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
+vim.cmd 'autocmd BufWritePost init.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
+
+-- Install plugins if first start
+if first_start == true then
+    require('plugins')
+    vim.cmd [[PackerInstall]]
+end
+
+-- Load plugins
+require('plugins')
 
 -- Sensible defaults
 require('settings')
-
--- Install plugins
-require('plugins')
 
 -- Key mappings
 require('keymappings')
